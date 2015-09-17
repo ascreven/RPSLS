6394a1e2c9bc2fb559ca888bda45f2c41a2c4805
ascreven <akscreven@gmail.com>
2015-09-17 17:52:27 -0400

    Interactive Apps
    Implemented skills learned in week 2

--- RPSLS.py
+++ RPSLS.py
@@ -1,58 +1,68 @@
-
+import simplegui
 import random
+
+# Functions that compute RPSLS
 def name_to_number(name):
-   if (name=="rock"):
-    return 0;
-   elif (name=="Spock"):
-    return 1;
-   elif (name=="paper"):
-    return 2;
-   elif (name=="lizard"):
-    return 3;
-   elif (name=="scissors"):
-    return 4;
-   else:
-    return name 
+    if (name=="rock"):
+        return 0;
+    elif (name=="Spock"):
+        return 1;
+    elif (name=="paper"):
+        return 2;
+    elif (name=="lizard"):
+        return 3;
+    elif (name=="scissors"):
+        return 4;
+    else:
+        return 100;
 
 def number_to_name(number):
-   if (number==0):
-    return "rock";
-   elif (number==1):
-    return "Spock";
-   elif (number==2):
-    return "paper";
-   elif (number==3):
-    return "lizard";
-   elif (number==4):
-    return "scissors";
-   else:
-    return number
-    
-    return number_to_name
-    
+    if (number==0):
+        return "rock";
+    elif (number==1):
+        return "Spock";
+    elif (number==2):
+        return "paper";
+    elif (number==3):
+        return "lizard";
+    elif (number==4):
+        return "scissors";
+    else:
+        return number
 
-def rpsls(player_choice):
+
+def rpsls(guess):  
     print
-    print "Player chooses ",player_choice
-    player_number = name_to_number(player_choice)
-    comp_number = random.randrange(0,4)
-    comp_choice = number_to_name(comp_number)
+    player_number=name_to_number(guess) 
+    comp_number = random.randint(0,5)
+    comp_choice=number_to_name(comp_number);
+    if (player_number==100):
+        return "Bad input "+guess+" to rpsls"
+    
+    print "Player chooses",guess
     print "Computer chooses ",comp_choice
-    winner=(player_number-comp_number)%5
-    if (winner==2 or winner==4):
-        print "Player wins!"
-    elif (winner==1 or winner==3):
-        print "Computer wins!"
-    else: 
-        print "Player and computer tie!"
-
+    if ((player_number-comp_number)%5==1) or ((player_number-comp_number)%5==2):
+        return "Player wins!"
+    elif ((player_number-comp_number)%5==3) or ((player_number-comp_number)%5==4):
+        return "Computer wins!"
+    elif ((player_number-comp_number)%5==0): 
+        return "Player and computer tie!"
 
     
-rpsls("rock")
-rpsls("Spock")
-rpsls("paper")
-rpsls("lizard")
-rpsls("scissors")
+# Handler for input field
+def get_guess(guess):
+    print rpsls(guess)
+    
 
+    
+
+
+# Create frame and assign callbacks to event handlers
+frame = simplegui.create_frame("GUI-based RPSLS", 200, 200)
+frame.add_input("Enter guess for RPSLS", get_guess, 200)
+
+
+# Start the frame animation
+frame.start()
 
 
